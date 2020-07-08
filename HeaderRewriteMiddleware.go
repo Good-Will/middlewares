b2ec2eb9cddb8e898f5471dee708814a50b909c3
Barry Qin <xiaoyu.qin@ieee.org>
2020-07-08 15:23:46 +1000

    Fix CORS header wildcard issue.

--- HeaderRewriteMiddleware.go
+++ HeaderRewriteMiddleware.go
@@ -19,9 +19,9 @@ func NewRequestHeaderWriteMiddlwware(headers map[string]string) func(next http.H
 
 // NewResponseHeaderWriteMiddlwware creates a middleware to rewrite HTTP headers of responses.
 func NewResponseHeaderWriteMiddlwware(headers map[string]string) func(next http.Handler) http.Handler {
-	rewriteHeader := castToHeader(headers)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rewriteHeader := castToHeaderForRequest(headers, r)
 			rw := rewriteResponseWriter{ResponseWriter: w, RewriteHeader: rewriteHeader}
 			next.ServeHTTP(rw, r)
 		})
@@ -32,6 +32,18 @@ func castToHeader(c map[string]string) http.Header {
 	rewriteHeader := make(http.Header)
 	for k, v := range c {
 		rewriteHeader[k] = []string{v}
+	}
+	return rewriteHeader
+}
+
+func castToHeaderForRequest(c map[string]string, r *http.Request) http.Header {
+	rewriteHeader := make(http.Header)
+	for k, v := range c {
+		if k == "Access-Control-Allow-Origin" && v == "*" {
+			rewriteHeader[k] = r.Header["Origin"]
+		} else {
+			rewriteHeader[k] = []string{v}
+		}
 	}
 	return rewriteHeader
 }
